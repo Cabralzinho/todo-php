bfb5b4906f5250ad53291934bbcd790f3590bec9
Cabral <thicabral2018@outlook.com>
2024-05-03 22:05:26 -0300

    feat: Adicionado o Core

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -1,5 +1,23 @@
-<?php 
+<?php
 
-class Core {
-    
+class Core
+{
+    public function start($urlGet)
+    {
+        if (isset($urlGet["pagina"])) {
+            $controller = ucfirst($urlGet["pagina"] . "Controller");
+        } else {
+            $controller = "HomeController";
+        }
+
+        $acao = "index";
+
+        if (!class_exists($controller)) {
+            $controller = "ErroController";
+        }
+
+        call_user_func_array([
+            new $controller, $acao
+        ], []);
+    }
 }
