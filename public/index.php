432ef4d1a98c7ebe188c4b293a8ddb1e043a1fa2
Cabral <thicabral2018@outlook.com>
2024-05-03 22:12:45 -0300

    feat: adiciona pagina index
    Adiciona a lógica de requires onde faz a lógica com o core chamando a instancia dele

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
-<?php 
+<?php
 
-$template = file_get_contents("../app/Template/todas_tarefas.php");
+require "../app/Core/Core.php";
+require "../app/Controller/HomeController.php";
+require "../app/Controller/ErroController.php";
+require "../app/Model/Tarefa.php";
+require "../lib/Database/Connection.php";
+
+$template = file_get_contents("../app/Template/nova_tarefa.php");
+
+$core = new Core();
+$core->start($_GET);
 
 echo $template;
